3bbb82d7f3a539c42aa0c2b6fa95ca9e60d2ccbb
lu1993 <cao_lu66@live.com>
2017-09-25 21:51:35 -0500

    code and resources

--- 3dprinting-index-construct/name-ticker-match/CompanyNameDownloader.py
+++ 3dprinting-index-construct/name-ticker-match/CompanyNameDownloader.py
@@ -8,33 +8,32 @@ Created on Fri Sep 22 14:26:26 2017
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
-import pandas as pd
 import re
-import pyodbc as p
-from datetime import datetime
-import os
-os.chdir('D:/Strategy/QuickCheck/SplitAnnouncement')
+import pandas as pd
+from pkg_resources import resource_filename
 
 
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# get companies from 3dprintingbusiness
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-companies = []
-home_url = "https://www.3dprintingbusiness.directory/companies/"
-for k in np.arange(1,186,1):
-    if k == 1:
-        url = home_url
-    else:
-        url = home_url + "page/" + str(k) + "/"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    company = soup.find_all('div', class_='listing-title')
-    companies.append(company)
-    
 def striphtml(data):
     comp = re.compile(r'<.*?>')
     return comp.sub('', data)
+    
 
-companies = [striphtml(str(x)) for x in company] 
+def main():
+    companies = []
+    home_url = "https://www.3dprintingbusiness.directory/companies/"
+    for k in np.arange(1,186,1):
+        if k == 1:
+            url = home_url
+        else:
+            url = home_url + "page/" + str(k) + "/"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        company = soup.find_all('div', class_='listing-title')
+        companies.append(company)
+    
+    companies = [striphtml(str(x)) for x in company] 
+    pd.DataFrame({'Name':companies}).to_csv(resource_filename('name-ticker-match.resources', 'CompanyName.csv'), index = False)
+    
+  
+if __name__ == '__main__':
+    main()
